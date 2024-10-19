94e91210799a2ec02df8160f41fc667792df317f
1GreenTea <rgreentear@gmail.com>
2024-10-19 18:56:27 +0300

    .

--- obstacle.js
+++ obstacle.js
@@ -1,43 +1,56 @@
 class Obstacle {
-    width = 50;
+    size = 50;
     holeSize = 150;
-    pos;
+    x;
     holeY;
 
     constructor(pos = 0.5, holeY = 0.5) {
-        this.pos = pos;
+        this.x = pos;
         this.holeY = holeY;
     }
-    
+
     update() {
-        this.pos -= 0.0015;
+        this.x -= 0.0015;
+        this.collision();
+    }
+
+    collision() {
+        const e1 = player;
+        const e2 = this;
+
+        const distanceX = Math.abs(e1.x - e2.x) * config.width;
+
+        if (distanceX < e1.size / 2 + e2.size / 2) {
+        }
     }
 
     render() {
-        const obstacleWidth = this.width * state.scale;
+        const obstacleWidth = this.size * state.scale;
         const holeSize = this.holeSize * state.scale;
+        const offsetTop = holeSize / 2;
+        const offsetBottom = holeSize / 2;
+        const calculatedY = (state.height - (offsetTop + offsetBottom)) * this.holeY + offsetTop;
+
+        if (this.collision()) {
+            c.fillStyle = 'blue';
+        } else {
+            c.fillStyle = 'red';
+        }
 
         c.fillRect(
-            (state.width * this.pos) - obstacleWidth * 0.5,
+            state.width * this.x - obstacleWidth * 0.5,
             0,
             obstacleWidth,
-            holeSize / 2 + ((state.height - holeSize) * this.holeY) - (holeSize / 2)
-          );
+            calculatedY - offsetTop
+        );
 
-          c.fillRect(
-            (state.width * this.pos) - obstacleWidth * 0.5,
-            holeSize / 2 + ((state.height - holeSize) * this.holeY) + (holeSize / 2),
+        c.fillRect(
+            state.width * this.x - obstacleWidth * 0.5,
+            calculatedY + offsetTop,
             obstacleWidth,
-            state.height - (holeSize / 2 + ((state.height - holeSize) * this.holeY) + (holeSize / 2))
-          );
+            state.height - (calculatedY + offsetTop)
+        );
 
-          c.fillRect(
-            (state.width * this.pos) - 5,
-            holeSize / 2 + ((state.height - holeSize) * this.holeY) - 5,
-            10,
-            10
-          );
-
-
+        c.fillRect(state.width * this.x - 5, calculatedY - 5, 10, 10);
     }
 }
