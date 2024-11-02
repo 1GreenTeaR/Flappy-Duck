72412944ea4d8ca0831c888e42f3a70818f72505
1GreenTea <rgreentear@gmail.com>
2024-11-02 19:15:24 +0200

    .

--- obstacle.js
+++ obstacle.js
@@ -1,12 +1,14 @@
 class Obstacle {
     size = 50;
     holeSize = 150;
+    offsetTop = 75;
+    offsetBottom = 75;
     x;
-    holeY;
+    y;
 
     constructor(pos = 0.5, holeY = 0.5) {
         this.x = pos;
-        this.holeY = holeY;
+        this.y = holeY;
     }
 
     update() {
@@ -21,15 +23,38 @@ class Obstacle {
         const distanceX = Math.abs(e1.x - e2.x) * config.width;
 
         if (distanceX < e1.size / 2 + e2.size / 2) {
+            const e1Pos = { x: e1.x * config.width, y: e1.y * config.height };
+            const e2Pos = {
+                x: this.x * config.width,
+                y: (config.height - (this.offsetTop + this.offsetBottom)) * this.y + this.offsetTop,
+            };
+
+            const points = [
+                { x: e2Pos.x - this.size / 2, y: e2Pos.y - this.holeSize / 2 },
+                { x: e2Pos.x + this.size / 2, y: e2Pos.y - this.holeSize / 2 },
+                { x: e2Pos.x + this.size / 2, y: e2Pos.y + this.holeSize / 2 },
+                { x: e2Pos.x - this.size / 2, y: e2Pos.y + this.holeSize / 2 },
+            ];
+
+            console.log(e2Pos, points);
+
+            for (let i = 0; i < points.length; i++) {
+                if (
+                    player.size / 2 >
+                    ((points[i].x - e1Pos.x) ** 2 + (points[i].y - e1Pos.y) ** 2) ** 0.5
+                ) {
+                    return true;
+                }
+            }
         }
     }
 
     render() {
         const obstacleWidth = this.size * state.scale;
         const holeSize = this.holeSize * state.scale;
-        const offsetTop = holeSize / 2;
-        const offsetBottom = holeSize / 2;
-        const calculatedY = (state.height - (offsetTop + offsetBottom)) * this.holeY + offsetTop;
+        const offsetTop = this.offsetTop * state.scale;
+        const offsetBottom = this.offsetBottom * state.scale;
+        const calculatedY = (state.height - (offsetTop + offsetBottom)) * this.y + offsetTop;
 
         if (this.collision()) {
             c.fillStyle = 'blue';
@@ -41,12 +66,12 @@ class Obstacle {
             state.width * this.x - obstacleWidth * 0.5,
             0,
             obstacleWidth,
-            calculatedY - offsetTop
+            calculatedY - holeSize / 2
         );
 
         c.fillRect(
             state.width * this.x - obstacleWidth * 0.5,
-            calculatedY + offsetTop,
+            calculatedY + holeSize / 2,
             obstacleWidth,
             state.height - (calculatedY + offsetTop)
         );
